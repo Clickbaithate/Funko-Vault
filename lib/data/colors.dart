22a45cee2842d4c3f9f56d9268c9a384c2692130
Clickbaithate <gg.guzzman1514@gmail.com>
2024-06-22 19:57:40 -0500

    Pushing const Colors and List of Series

---
+++ lib/data/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xffEAE7EA);
+const Color secondaryColor = Color(0xff000000);
+const Color backgroundColor = Color(0xffFFFFFF);
+const Color blueColor = Color(0xff0075FF);
+const Color redColor = Color(0xffFF0000);
+const Color biegeColor = Color(0xffB99976);
