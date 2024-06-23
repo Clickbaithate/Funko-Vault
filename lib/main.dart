9a93395d95fafd61ca1f1d261b9fcdf568265d6a
Clickbaithate <gg.guzzman1514@gmail.com>
2024-06-22 21:30:56 -0500

    Setup main to start with BottomNav and Gave network permissions for release build

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:temp/models/bottom_nav.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: "FunkoVault",
+      home: BottomNav(),
     );
   }
 }
